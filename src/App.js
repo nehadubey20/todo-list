d574e1eea690f5c847d03906b8d34e69a7897246
Neha Dubey <008nehadubey@gmail.com>
2020-10-20 00:32:24 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends React.Component{
       });
     }
   }
-
+  //delete Item
   deleteItem(id){
     const list = [...this.state.list];
     const updatedList =  list.filter(item => item.id !== id);
