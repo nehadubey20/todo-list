8b75e6f66ce5b2fe8a2ac4f9eccbf2eaa24be037
Neha Dubey <008nehadubey@gmail.com>
2020-10-20 00:20:46 +0530

    Updates

---
+++ precache-manifest.b39abf08fd944318b1c8b134f72dfbe4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83098cc63de40ea4d71a7c31ec231341",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdde595d735015a6f94d",
+    "url": "/static/css/main.29b4d19c.chunk.css"
+  },
+  {
+    "revision": "46d8dbc974f4fa6fcdda",
+    "url": "/static/js/2.48d11504.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.48d11504.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdde595d735015a6f94d",
+    "url": "/static/js/main.db0cec78.chunk.js"
+  },
+  {
+    "revision": "559ff28579f192bdf8b9",
+    "url": "/static/js/runtime-main.98bec8e4.js"
+  }
+]);
